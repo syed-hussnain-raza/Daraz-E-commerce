8787750faa39e0a3da375998fb8b5dddfb9ebfaf
Syed-Muhammad-Hussnain-Raza <hussnain.fyp26@hazelsoft.org>
2026-05-04 00:08:12 +0500

    update: fetching data now using api instead of locally.

--- js/services/productService.js
+++ js/services/productService.js
@@ -1,9 +1,5 @@
 async function fetchProductData(id) {
-  const res = await fetch("../data/products.json");
-  if (!res.ok) throw new Error("Failed to load products.json");
-  const data = await res.json();
-  return (
-    data.products.find((p) => p.id === id) ||
-    data.products.find((p) => p.id === "dustproof")
-  );
+  const res = await fetch(`http://localhost:3000/api/products/${id}`);
+  if (!res.ok) throw new Error("Failed to load product");
+  return res.json();
 }
